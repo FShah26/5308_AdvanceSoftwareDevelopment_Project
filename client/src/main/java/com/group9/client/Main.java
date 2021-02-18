469f67fd320e4b99e3d6d257633e61df3040895a
HASHIK DONTHINENI <hs925208@dal.ca>
2021-02-18 13:31:35 +0530

    Migrated Client to Maven and added tests

---
+++ client/src/main/java/com/group9/client/Main.java
@@ -0,0 +1,13 @@
+package com.group9.client;
+
+public class Main {
+
+    // Just to add a test to start the Gitlab pipeline
+    public static String hello(String hi){
+        return hi;
+    }
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
