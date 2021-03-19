09116c6c7524f48a0bed3e29c6899fdf62624c0c
HASHIK DONTHINENI <hs925208@dal.ca>
2021-03-20 00:20:43 +0530

    Added ViewAnnouncement tests, empty class

---
+++ server/src/main/java/com/group9/server/Announcements/Student/ViewAnnouncementsImpl.java
@@ -0,0 +1,13 @@
+package com.group9.server.Announcements.Student;
+
+public class ViewAnnouncementsImpl implements ViewAnnouncements {
+    @Override
+    public AnnouncementList fetchAnnouncements() {
+        return null;
+    }
+
+    @Override
+    public void displayAllAnnouncements() {
+
+    }
+}
---
+++ server/src/test/java/com/group9/server/Announcements/Student/ViewAnnouncementsTest.java
@@ -0,0 +1,29 @@
+package com.group9.server.Announcements.Student;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ViewAnnouncementsTest {
+    ViewAnnouncements underTest;
+
+    @BeforeEach
+    void setUp() {
+        underTest = new ViewAnnouncementsImpl();
+    }
+
+    @Test
+    @DisplayName("fetchAnnouncementsTest")
+    void fetchAnnouncementsTest() {
+        Assertions.assertNotNull(underTest.fetchAnnouncements());
+    }
+
+    @Test
+    @DisplayName("displayAllAnnouncementsTest")
+    void displayAllAnnouncementsTest() {
+        underTest.displayAllAnnouncements();
+    }
+}
