443b6f8100edbb34c30402da9f10d2eb9c651fe6
HASHIK DONTHINENI <hs925208@dal.ca>
2021-03-20 00:06:44 +0530

    Added single announcement tests

--- server/src/main/java/com/group9/server/Announcements/Student/SingleAnnouncement.java
+++ server/src/main/java/com/group9/server/Announcements/Student/SingleAnnouncement.java
@@ -2,4 +2,6 @@ package com.group9.server.Announcements.Student;
 
 public interface SingleAnnouncement {
     void printAnnouncement();
+    void setUserID(String id);
+    void setAnnouncement(String announcement);
 }
---
+++ server/src/main/java/com/group9/server/Announcements/Student/SingleAnnouncementImpl.java
@@ -0,0 +1,22 @@
+package com.group9.server.Announcements.Student;
+
+public class SingleAnnouncementImpl implements SingleAnnouncement{
+    private String userID;
+    private String announcement;
+
+
+    @Override
+    public void printAnnouncement() {
+
+    }
+
+    @Override
+    public void setUserID(String id) {
+
+    }
+
+    @Override
+    public void setAnnouncement(String announcement) {
+
+    }
+}
