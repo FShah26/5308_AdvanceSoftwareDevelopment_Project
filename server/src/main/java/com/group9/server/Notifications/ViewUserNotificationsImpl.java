656e58ed7cd368ffb78a94e75992816db591dcca
HASHIK DONTHINENI <hs925208@dal.ca>
2021-03-28 13:18:24 +0530

    Added test for notificationPersistence

--- server/src/main/java/com/group9/server/Notifications/ViewUserNotificationsImpl.java
+++ server/src/main/java/com/group9/server/Notifications/ViewUserNotificationsImpl.java
@@ -3,6 +3,6 @@ package com.group9.server.Notifications;
 public class ViewUserNotificationsImpl implements ViewUserNotifications {
     @Override
     public void displayAllNotifications(String user) {
-        
+
     }
 }
