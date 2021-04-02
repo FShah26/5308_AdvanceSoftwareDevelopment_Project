939e9c77576d9877194b0b428e7915a7d930a2cb
Fenil Shah <fn777214@dal.ca>
2021-04-03 02:37:15 +0530

    Implemented announcement feature for a faculty

--- server/src/main/java/com/group9/server/Announcements/Admin/IAnnouncementLogic.java
+++ server/src/main/java/com/group9/server/Announcements/Admin/IAnnouncementLogic.java
@@ -1,5 +1,6 @@
 package com.group9.server.Announcements.Admin;
 
 public interface IAnnouncementLogic {
-    String make_announcement(String userRole, String message, String userId);
+    String make_announcement(String userRole,String courseId, String message, String userId);
+    int validateCourseId(String facultyId, String courseId);
 }
