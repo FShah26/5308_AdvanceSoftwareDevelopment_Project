00e00b5028a6ab43385f72eb45b02a0323743a06
Utkarsh Patel <ut436516@dal.ca>
2021-04-04 17:29:35 +0530

    Fixed some presentation layer

--- server/src/main/java/com/group9/server/UserCreation/AddUserLogic.java
+++ server/src/main/java/com/group9/server/UserCreation/AddUserLogic.java
@@ -1,25 +1,40 @@
 package com.group9.server.UserCreation;
 
+import com.group9.server.Feedback.IFeedbackPersistence;
 import org.springframework.stereotype.Component;
+
+import java.sql.SQLException;
 
 @Component
 public class AddUserLogic implements IAddUserLogic {
 
-    IAddUserPersistence addUserPersistence;
+    IAddUserPersistence persistence;
 
-    public void AddUserLogic(IAddUserPersistence addUserPersistence) {
-        this.addUserPersistence = addUserPersistence;
+    public AddUserLogic(IAddUserPersistence persistence) {
+        this.persistence = persistence;
     }
 
     @Override
-    public void addUser(String id, String userId, String password, String userType) {
-        addUserPersistence.addUser(id, userId, password, userType);
+    public String addUser(String id, String userId, String password, String userType) {
+        String message = "Added user";
+        try {
+            persistence.addUser(id, userId, password, userType);
+        } catch (Exception e) {
+            System.out.println("Adding feedback failed");
+            e.printStackTrace();
+        }    return message;
     }
 
     @Override
-    public void addUserDetails(String userId, String userType, String name, String emailAddress, String department) {
-        addUserPersistence.addUserDetails(userId, userType, name, emailAddress, department);
+    public String addUserDetails(String userId, String userType, String name, String emailAddress, String department) {
+        String message = "Added user details";
+        try{
+            persistence.addUserDetails(userId, userType, name, emailAddress, department);
+        }catch (Exception e){
+            System.out.println("Failed");
+            e.printStackTrace();
+        }
+        return message;
     }
-
 }
 
