0cf552f348b3ee26b154675928150ff4a2bcdd27
HASHIK DONTHINENI <hs925208@dal.ca>
2021-04-07 11:47:31 +0530

    Refactored ValidateAnnouncementMade

--- server/src/main/java/com/group9/server/Announcements/Admin/ValidateAnnouncementMade.java
+++ server/src/main/java/com/group9/server/Announcements/Admin/ValidateAnnouncementMade.java
@@ -4,8 +4,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ValidateAnnouncementMade implements IValidateAnnouncementMade {
+    final int MAX_ALLOWED_MSG_LENGTH = 2000;
+
     @Override
     public Boolean validateAnnouncement(String message) {
-        return message.length() < 2000;
+        return message.length() < MAX_ALLOWED_MSG_LENGTH;
     }
 }
